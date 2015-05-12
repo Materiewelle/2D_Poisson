0bd2df5d54c58c19bc5ca002225366ba00065288
Tobias Linn <tobias.linn@rwth-aachen.de>
2015-05-12 15:46:36 +0200

    machma mullllll

--- main.cpp
+++ main.cpp
@@ -23,10 +23,8 @@ int main() {
     static constexpr double dr2 = 1.0 / d::dr / d::dr;
     static constexpr double dx2 = 1.0 / d::dx / d::dx;
 
-    double mull = 6.5;
+    double mull = 1;
     int i = 0;
-    double r = 0;
-
     sp_mat left, left2, center, center2, right, right2;
 
     // get the size of the complete sparse matrix:
@@ -40,6 +38,7 @@ int main() {
     sp_mat S(D, D);
 
     // cnt start
+    double r = 0;
     center = sp_mat(d::N_x, d::N_x);
     center.diag(0).fill(mull);
     center.diag( 1).fill(mull);
@@ -47,18 +46,16 @@ int main() {
     center(0, 1) *= 2;
     center(d::N_x - 1, d::N_x - 2) *= 2;
     right = sp_mat(d::N_x, d::N_x);
-    right.diag().fill(mull);
-
+    right.diag().fill(2 * mull);
     uword j0 = 0;
     uword j1 = d::N_x - 1;
     S({j0, j1}, {j0         , j1         }) = center;
     S({j0, j1}, {j0 + d::N_x, j1 + d::N_x}) = right;
 
-    mull = 3.2;
+    mull = 2;
     // cnt mid
     for (i = 1; i < d::M_cnt - 1; ++i) {
         r = i * d::dr;
-
         left = sp_mat(d::N_x, d::N_x);
         left.diag().fill(mull);
         center = sp_mat(d::N_x, d::N_x);
@@ -69,7 +66,6 @@ int main() {
         center(d::N_x - 1, d::N_x - 2) *= 2;
         right = sp_mat(d::N_x, d::N_x);
         right.diag().fill(mull);
-
         j0 = j1 + 1;
         j1 += d::N_x;
         S({j0, j1}, {j0 - d::N_x, j1 - d::N_x}) = left;
@@ -77,7 +73,7 @@ int main() {
         S({j0, j1}, {j0 + d::N_x, j1 + d::N_x}) = right;
     }
 
-    mull = 8.2;
+    mull = 3;
     // cnt end
     r = i * d::dr;
     left = sp_mat(d::N_x, d::N_x);
@@ -90,7 +86,6 @@ int main() {
     center(d::N_x - 1, d::N_x - 2) *= 2;
     right = sp_mat(d::N_x, N_ox);
     right.diag(-d::N_sc).fill(mull);
-
     j0 = j1 + 1;
     j1 += d::N_x;
     S({j0, j1}, {j0 - d::N_x, j1 - d::N_x}) = left;
@@ -98,7 +93,7 @@ int main() {
     S({j0, j1}, {j0 + d::N_x, j1 + N_ox  }) = right;
     ++i;
 
-    mull = 1.2;
+    mull = 4;
     // ox start
     r = i * d::dr;
     left = sp_mat(N_ox, d::N_x);
@@ -109,7 +104,6 @@ int main() {
     center.diag(-1).fill(mull);
     right = sp_mat(N_ox, N_ox);
     right.diag().fill(mull);
-
     j0 = j1 + 1;
     j1 += N_ox;
     S({j0, j1}, {j0 - d::N_x, j1 - N_ox}) = left;
@@ -117,7 +111,7 @@ int main() {
     S({j0, j1}, {j0 + N_ox  , j1 + N_ox}) = right;
     ++i;
 
-    mull = 9.1;
+    mull = 5;
     // ox mid
     for (; i < d::M_cnt + d::M_ox - 1; ++i) {
         r = i * d::dr;
@@ -129,7 +123,6 @@ int main() {
         center.diag(-1).fill(mull);
         right = sp_mat(N_ox, N_ox);
         right.diag().fill(mull);
-
         j0 = j1 + 1;
         j1 += N_ox;
         S({j0, j1}, {j0 - N_ox, j1 - N_ox}) = left;
@@ -137,7 +130,7 @@ int main() {
         S({j0, j1}, {j0 + N_ox, j1 + N_ox}) = right;
     }
 
-    mull = 2.2;
+    mull = 6;
     // ox end
     r = i * d::dr;
     left = sp_mat(N_ox, N_ox);
@@ -150,42 +143,111 @@ int main() {
     right.diag().fill(mull);
     right2 = sp_mat(N_dext, N_dext);
     right2.diag().fill(mull);
-
     j0 = j1 + 1;
     j1 += N_ox;
     S({j0, j1}, {j0 - N_ox, j1 - N_ox}) = left;
     S({j0, j1}, {j0       , j1       }) = center;
-    j1 = j0 + N_sext - 1;
     uword j2 = j1 - N_dext + 1;
     uword j3 = j1;
+    j1 = j0 + N_sext - 1;
     S({j0, j1}, {j0 + N_ox, j1 + N_ox}) = right;
     S({j2, j3}, {j3 + N_sext + 1, j3 + N_sext + N_dext}) = right2;
     ++i;
 
-    mull = 6.9;
+    mull = 7;
     // ext start
     r = i * d::dr;
     left = sp_mat(N_sext, N_sext);
     left.diag().fill(mull);
     left2 = sp_mat(N_dext, N_dext);
-    left2.diag().fill(mull);
+    left2.diag().fill(mull*2);
     center = sp_mat(N_sext, N_sext);
     center.diag( 0).fill(mull);
     center.diag( 1).fill(mull);
     center.diag(-1).fill(mull);
     center2 = sp_mat(N_dext, N_dext);
-    center2.diag( 0).fill(mull);
-    center2.diag( 1).fill(mull);
-    center2.diag(-1).fill(mull);
+    center2.diag( 0).fill(mull*2);
+    center2.diag( 1).fill(mull*2);
+    center2.diag(-1).fill(mull*2);
     right = sp_mat(N_sext, N_sext);
     right.diag().fill(mull);
     right2 = sp_mat(N_dext, N_dext);
-    right2.diag().fill(mull);
+    right2.diag().fill(mull*2);
+
+    j0 += N_ox;
+    j1 += N_ox;
+    j2 += N_sext + N_dext;
+    j3 += N_sext + N_dext;
+    S({j0, j1}, {j0 - N_ox, j1 - N_ox}) = left;
+    S({j0, j1}, {j0         , j1         }) = center;
+    S({j0, j1}, {j0 + N_sext + N_dext, j1 + N_sext + N_dext}) = right;
+    S({j2, j3}, {j2 - N_sext - N_dext, j3 - N_sext - N_dext}) = left2;
+    S({j2, j3}, {j2                  , j3}) = center2;
+    S({j2, j3}, {j2 + N_sext + N_dext, j3 + N_sext + N_dext}) = right2;
+    ++i;
+
+    mull = 10;
+    // ext mid
+    for (; i < d::M_cnt + d::M_ox + d::M_ext - 1; ++i) {
+        r = i * d::dr;
+        left = sp_mat(N_sext, N_sext);
+        left.diag().fill(mull);
+        left2 = sp_mat(N_dext, N_dext);
+        left2.diag().fill(mull*2);
+        center = sp_mat(N_sext, N_sext);
+        center.diag( 0).fill(mull);
+        center.diag( 1).fill(mull);
+        center.diag(-1).fill(mull);
+        center2 = sp_mat(N_dext, N_dext);
+        center2.diag( 0).fill(mull*2);
+        center2.diag( 1).fill(mull*2);
+        center2.diag(-1).fill(mull*2);
+        right = sp_mat(N_sext, N_sext);
+        right.diag().fill(mull);
+        right2 = sp_mat(N_dext, N_dext);
+        right2.diag().fill(mull*2);
+
+        j0 += N_sext + N_dext;
+        j1 += N_sext + N_dext;
+        j2 += N_sext + N_dext;
+        j3 += N_sext + N_dext;
+        S({j0, j1}, {j0 - N_sext - N_dext, j1 - N_sext - N_dext}) = left;
+        S({j0, j1}, {j0                  , j1                  }) = center;
+        S({j0, j1}, {j0 + N_sext + N_dext, j1 + N_sext + N_dext}) = right;
+        S({j2, j3}, {j2 - N_sext - N_dext, j3 - N_sext - N_dext}) = left2;
+        S({j2, j3}, {j2                  , j3                  }) = center2;
+        S({j2, j3}, {j2 + N_sext + N_dext, j3 + N_sext + N_dext}) = right2;
+    }
+
+    mull = 12;
+    // ext end
+    r = i * d::dr;
+    left = sp_mat(N_sext, N_sext);
+    left.diag().fill(mull*2);
+    left2 = sp_mat(N_dext, N_dext);
+    left2.diag().fill(mull*3);
+    center = sp_mat(N_sext, N_sext);
+    center.diag( 0).fill(mull);
+    center.diag( 1).fill(mull);
+    center.diag(-1).fill(mull);
+    center2 = sp_mat(N_dext, N_dext);
+    center2.diag( 0).fill(mull*2);
+    center2.diag( 1).fill(mull*2);
+    center2.diag(-1).fill(mull*2);
+    j0 += N_sext + N_dext;
+    j1 += N_sext + N_dext;
+    j2 += N_sext + N_dext;
+    j3 += N_sext + N_dext;
+    S({j0, j1}, {j0 - N_sext - N_dext, j1 - N_sext - N_dext}) = left;
+    S({j0, j1}, {j0                  , j1                  }) = center;
+    S({j2, j3}, {j2 - N_sext - N_dext, j3 - N_sext - N_dext}) = left2;
+    S({j2, j3}, {j2                  , j3                  }) = center2;
 
 
     gnuplot gp;
-    gp << "set terminal wxt" << endl;
-    gp.set_background(mat(S));
+    gp << "set terminal wxt size 640, 640" << endl;
+    gp << "unset colorbox" << endl;
+    gp.set_background(flipud(mat(S)));
     gp.plot();
 
 
