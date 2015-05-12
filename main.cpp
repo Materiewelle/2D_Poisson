7a1a73766573373fbbb14ce4b4daa672f81d91b0
Tobias Linn <tobias.linn@rwth-aachen.de>
2015-05-12 14:17:11 +0200

    ...

--- main.cpp
+++ main.cpp
@@ -22,68 +22,161 @@ int main() {
     static constexpr double dr2 = 1.0 / d::dr / d::dr;
     static constexpr double dx2 = 1.0 / d::dx / d::dx;
 
+    double mull = 6.5;
+    int i = 0;
+    double r = 0;
+
+    sp_mat left, left2, center, center2, right, right2;
+
     // get the size of the complete sparse matrix:
+    int N_ox = d::N_x - d::N_sc - d::N_dc;
+    int N_sext = d::N_s + d::N_sox;
+    int N_dext = d::N_d + d::N_dox;
     int D = 0;
     D += d::N_x * d::M_cnt;
-    D += (d::N_x - d::N_sc - d::N_dc) * d::M_ox;
-    D += ((d::N_s + d::N_sox) + (d::N_d + d::N_dox)) * d::M_ext;
+    D +=   N_ox * d::M_ox;
+    D += (N_sext + N_dext) * d::M_ext;
     sp_mat S(D, D);
 
-    // first row of blocks
-    sp_mat S_00(d::N_x, d::N_x);
-    S_00.diag(0).fill(-2.0 * (dr2 + dx2));
-    S_00.diag( 1).fill(dx2);
-    S_00.diag(-1).fill(dx2);
-    S_00(0, 1) *= 2;
-    S_00(d::N_x - 1, d::N_x - 2) *= 2;
-    S({0, d::N_x-1}, {0, d::N_x-1}) = S_00;
-
-    sp_mat S01(d::N_x, d::N_x);
-    S01.diag().fill(2.0 * dr2);
-    S({0, d::N_x-1},{d::N_x, 2*d::N_x-1}) = S01;
-
-    // central rows of blocks
-    for (int i = 1; i < d::M_cnt - 1; ++i) {
-        double r = i * d::dr;
-
-        sp_mat sides(d::N_x, d::N_x);
-        sides.diag().fill(dr2 - 0.5 / r / d::dr);
-
-        sp_mat center = sp_mat(d::N_x, d::N_x);
-        center.diag( 0).fill(-2.0 * (dr2 + dx2));
-        center.diag( 1).fill(dx2);
-        center.diag(-1).fill(dx2);
-        center(0, 1) *= 2;
-        center(d::N_x - 1, d::N_x - 2) *= 2;
-
-        S({i * d::N_x, (i+1) * d::N_x - 1}, {(i-1) * d::N_x, i     * d::N_x - 1}) = sides;
-        S({i * d::N_x, (i+1) * d::N_x - 1}, {i     * d::N_x, (i+1) * d::N_x - 1}) = center;
-        S({i * d::N_x, (i+1) * d::N_x - 1}, {(i+1) * d::N_x, (i+2) * d::N_x - 1}) = sides;
-    }
-
-    // transition from nanotube to gate-oxide
-    int i = d::M_cnt - 1;
-    sp_mat left(d::N_x, d::N_x);
-    double r = i * d::dr;
-    left.diag().fill(dr2 - 0.5 / r / d::dr);
-
-    sp_mat center(d::N_x, d::N_x);
-    center.diag(0).fill(-2.0 * (dr2 + dx2));
-    center.diag( 1).fill(dx2);
-    center.diag(-1).fill(dx2);
+    // cnt start
+    center = sp_mat(d::N_x, d::N_x);
+    center.diag(0).fill(mull);
+    center.diag( 1).fill(mull);
+    center.diag(-1).fill(mull);
     center(0, 1) *= 2;
     center(d::N_x - 1, d::N_x - 2) *= 2;
+    right = sp_mat(d::N_x, d::N_x);
+    right.diag().fill(mull);
 
-    int N_ox = d::N_x - d::N_sc - d::N_dc;
-    sp_mat right(N_ox, d::N_x);
-    right.diag(-d::N_sc).fill(5); //dummy-value
+    uword j0 = 0;
+    uword j1 = d::N_x - 1;
+    S({j0, j1}, {j0         , j1         }) = center;
+    S({j0, j1}, {j0 + d::N_x, j1 + d::N_x}) = right;
 
-    S({i * d::N_x, (i+1) * d::N_x - 1}, {(i-1) * d::N_x, i     * d::N_x - 1}) = left;
-    S({i * d::N_x, (i+1) * d::N_x - 1}, {i     * d::N_x, (i+1) * d::N_x - 1}) = center;
-    S({i * d::N_x, (i+1) * d::N_x - 1}, {(i+1) * d::N_x, (i+1) * d::N_x + d::N_sc - 1}) = right;
+    // cnt mid
+    for (i = 1; i < d::M_cnt - 1; ++i) {
+        r = i * d::dr;
+
+        left = sp_mat(d::N_x, d::N_x);
+        left.diag().fill(mull);
+        center = sp_mat(d::N_x, d::N_x);
+        center.diag( 0).fill(mull);
+        center.diag( 1).fill(mull);
+        center.diag(-1).fill(mull);
+        center(0, 1) *= 2;
+        center(d::N_x - 1, d::N_x - 2) *= 2;
+        right = sp_mat(d::N_x, d::N_x);
+        right.diag().fill(mull);
+
+        j0 = j1 + 1;
+        j1 += d::N_x;
+        S({j0, j1}, {j0 - d::N_x, j1 - d::N_x}) = left;
+        S({j0, j1}, {j0         , j1         }) = center;
+        S({j0, j1}, {j0 + d::N_x, j1 + d::N_x}) = right;
+    }
+
+    // cnt end
+    r = i * d::dr;
+    left = sp_mat(d::N_x, d::N_x);
+    left.diag().fill(mull);
+    center = sp_mat(d::N_x, d::N_x);
+    center.diag( 0).fill(mull);
+    center.diag( 1).fill(mull);
+    center.diag(-1).fill(mull);
+    center(0, 1) *= 2;
+    center(d::N_x - 1, d::N_x - 2) *= 2;
+    right = sp_mat(d::N_x, N_ox);
+    right.diag(-d::N_sc).fill(mull);
+
+    j0 = j1 + 1;
+    j1 += d::N_x;
+    S({j0, j1}, {j0 - d::N_x, j1 - d::N_x}) = left;
+    S({j0, j1}, {j0         , j1         }) = center;
+    S({j0, j1}, {j0 + d::N_x, j1 + N_ox  }) = right;
+    ++i;
+
+    // ox start
+    r = i * d::dr;
+    left = sp_mat(N_ox, d::N_x);
+    left.diag(d::N_sc).fill(mull);
+    center = sp_mat(N_ox, N_ox);
+    center.diag( 0).fill(mull);
+    center.diag( 1).fill(mull);
+    center.diag(-1).fill(mull);
+    right = sp_mat(N_ox, N_ox);
+    right.diag().fill(mull);
+
+    j0 = j1 + 1;
+    j1 += N_ox;
+    S({j0, j1}, {j0 - d::N_x, j1 - N_ox}) = left;
+    S({j0, j1}, {j0         , j1       }) = center;
+    S({j0, j1}, {j0 + N_ox  , j1 + N_ox}) = right;
+    ++i;
+
+    // ox mid
+    for (; i < d::M_cnt + d::M_ox - 1; ++i) {
+        r = i * d::dr;
+        left = sp_mat(N_ox, N_ox);
+        left.diag().fill(mull);
+        center = sp_mat(N_ox, N_ox);
+        center.diag( 0).fill(mull);
+        center.diag( 1).fill(mull);
+        center.diag(-1).fill(mull);
+        right = sp_mat(N_ox, N_ox);
+        right.diag().fill(mull);
+
+        j0 = j1 + 1;
+        j1 += N_ox;
+        S({j0, j1}, {j0 - N_ox, j1 - N_ox}) = left;
+        S({j0, j1}, {j0       , j1       }) = center;
+        S({j0, j1}, {j0 + N_ox, j1 + N_ox}) = right;
+    }
+
+    // ox end
+    r = i * d::dr;
+    left = sp_mat(N_ox, N_ox);
+    left.diag().fill(mull);
+    center = sp_mat(N_ox, N_ox);
+    center.diag( 0).fill(mull);
+    center.diag( 1).fill(mull);
+    center.diag(-1).fill(mull);
+    right = sp_mat(N_sext, N_sext);
+    right.diag().fill(mull);
+    right2 = sp_mat(N_dext, N_dext);
+    right2.diag().fill(mull);
+
+    j0 = j1 + 1;
+    j1 += N_ox;
+    S({j0, j1}, {j0 - N_ox, j1 - N_ox}) = left;
+    S({j0, j1}, {j0       , j1       }) = center;
+    j1 = j0 + N_sext - 1;
+    uword j2 = ;
+    uword j3 = ;
+    S({j0, j1}, {j0 + N_ox, j1 + N_ox}) = right;
+    S({}, {}) = right2;
+    ++i;
+
+    // ext start
+    r = i * d::dr;
+    left = sp_mat(N_sext, N_sext);
+    left.diag().fill(mull);
+    left2 = sp_mat(N_dext, N_dext);
+    left2.diag().fill(mull);
+    center = sp_mat(N_sext, N_sext);
+    center.diag( 0).fill(mull);
+    center.diag( 1).fill(mull);
+    center.diag(-1).fill(mull);
+    center2 = sp_mat(N_dext, N_dext);
+    center2.diag( 0).fill(mull);
+    center2.diag( 1).fill(mull);
+    center2.diag(-1).fill(mull);
+    right = sp_mat(N_sext, N_sext);
+    right.diag().fill(mull);
+    right2 = sp_mat(N_dext, N_dext);
+    right2.diag().fill(mull);
+
 
     cout << S << endl;
-
 
     return 0;
 }
