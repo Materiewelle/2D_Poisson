4012732c0a1ebaa785a40d6d90c7f7e1def9233f
Fabian Motzfeld <fabian.motzfeld@rwth-aachen.de>
2015-06-08 11:18:34 +0200

    prepared tfet curve for obelix

--- main.cpp
+++ main.cpp
@@ -26,46 +26,22 @@ int main() {
     _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
     _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
 
-    device nfet("nfet digga", nfet_model, standard_geometry);
-    device pfet("pfet digga", pfet_model, standard_geometry);
+    device tfet("tfet", tfet_model, standard_geometry);
 
-//    steady_state s(n_fet, {0, 0.2, 0.5});
-//    s.solve();
-//    wave_packet psi;
-//    psi.init< true>(n_fet, s.E[LC], s.W[LC], s.phi);
+    vec V_g;
+    vec I;
+    steady_state::transfer(tfet, {0.0, -0.2, 0.4}, 0.6, 250, V_g, I);
 
-//    image(psi.E);
+    mat res = join_horiz(V_g, I);
+    res.save("tfet_transfer", raw_ascii);
 
-//    inverter i(n_fet, p_fet);
-//    vec V_in;
-//    vec V_out;
-//    i.output({0, 0.17, 0.4}, 0.23, 20, V_in, V_out);
-
-//    plot(make_pair(V_in, V_out));
-
-    time_evolution te(nfet);
-    std::fill(begin(te.V), begin(te.V) + 2, voltage{0.0, 0.2, 0.5});
-    vec ramp = linspace(0, 0.05, 20);
-    for (int i = 2; i < 22; ++i) {
-        te.V[i] = {ramp(i-2), 0.2, 0.5};
-    }
-    std::fill(begin(te.V) + 22, end(te.V), voltage{0.05, 0.2, 0.5});
-    te.solve();
-
-//    vec V_d;
-//    vec I;
-//    steady_state::output(p_fet, {0.0, -0.2, -0.6}, 0.2, 250, V_d, I);
-//    plot(make_pair(V_d, I));
-
-//    device der_geraet("der Gerät");
-
-//    voltage V0{0, .2, .5};
-//    vector<voltage> V(t::N_t);
-//    for (int i = 0; i < t::N_t; ++i) {
-//        V[i] = V0;
+//    time_evolution te(nfet);
+//    std::fill(begin(te.V), begin(te.V) + 2, voltage{0.0, 0.2, 0.5});
+//    vec ramp = linspace(0, 0.05, 20);
+//    for (int i = 2; i < 22; ++i) {
+//        te.V[i] = {ramp(i-2), 0.2, 0.5};
 //    }
-
-//    time_evolution te(der_geraet, V);
+//    std::fill(begin(te.V) + 22, end(te.V), voltage{0.05, 0.2, 0.5});
 //    te.solve();
 
     return 0;
