929ac0e14a29d25938ca0fa521a23d212db93c66
Tobias Linn <tobias.linn@rwth-aachen.de>
2015-06-11 10:56:12 +0200

    changed charge_density and green for new device

--- charge_density.hpp
+++ charge_density.hpp
@@ -201,116 +201,116 @@ charge_density::charge_density(const device & d, const wave_packet psi[4], const
 }
 
 arma::vec charge_density_impl::get_bound_states(const device & d, const potential & phi) {
-//    double phi0, phi1, phi2, limit;
+    double phi0, phi1, phi2, limit;
 
-//    // check for bound states in valence band
-//    phi0 = arma::min(phi.data(d.s)) - 0.5 * d.E_g;
-//    phi1 = arma::max(phi.data(d.g)) - 0.5 * d.E_g;
-//    phi2 = arma::min(phi.data(d.d)) - 0.5 * d.E_g;
-//    limit = phi0 > phi2 ? phi0 : phi2;
-//    if (limit < phi1) {
-//        return get_bound_states(d, phi, limit, phi1);
-//    }
+    // check for bound states in valence band
+    phi0 = arma::min(phi.data(d.sg)) - 0.5 * d.E_g;
+    phi1 = arma::max(phi.data(d.g )) - 0.5 * d.E_g;
+    phi2 = arma::min(phi.data(d.dg)) - 0.5 * d.E_g;
+    limit = phi0 > phi2 ? phi0 : phi2;
+    if (limit < phi1) {
+        return get_bound_states(d, phi, limit, phi1);
+    }
 
-//    // check for bound states in conduction band
-//    phi0 = arma::max(phi.data(d.s)) + 0.5 * d.E_g;
-//    phi1 = arma::min(phi.data(d.g)) + 0.5 * d.E_g;
-//    phi2 = arma::max(phi.data(d.d)) + 0.5 * d.E_g;
-//    limit = phi0 < phi2 ? phi0 : phi2;
-//    if (limit > phi1) {
-//        return get_bound_states(d, phi, phi1, limit);
-//    }
+    // check for bound states in conduction band
+    phi0 = arma::max(phi.data(d.sg)) + 0.5 * d.E_g;
+    phi1 = arma::min(phi.data(d.g )) + 0.5 * d.E_g;
+    phi2 = arma::max(phi.data(d.dg)) + 0.5 * d.E_g;
+    limit = phi0 < phi2 ? phi0 : phi2;
+    if (limit > phi1) {
+        return get_bound_states(d, phi, phi1, limit);
+    }
 
-//    return arma::vec(arma::uword(0));
+    return arma::vec(arma::uword(0));
 }
 
 arma::vec charge_density_impl::get_bound_states(const device & d, const potential & phi, double E0, double E1) {
-//    using namespace arma;
+    using namespace arma;
 
-//    static constexpr double tol = 1e-10;
+    static constexpr double tol = 1e-10;
 
-//    span range{d.s2.a, d.d2.b};
-//    vec a = d.t_vec(range);
-//    vec a2 = a % a;
-//    vec b = phi.twice(range);
+    span range{d.sg2.a, d.dg2.b};
+    vec a = d.t_vec(range);
+    vec a2 = a % a;
+    vec b = phi.twice(range);
 
-//    double E2;
-//    int i0, i1;
-//    int s0, s1, s2;
+    double E2;
+    int i0, i1;
+    int s0, s1, s2;
 
-//    s0 = eval(a, a2, b, E0);
-//    s1 = eval(a, a2, b, E1);
+    s0 = eval(a, a2, b, E0);
+    s1 = eval(a, a2, b, E1);
 
-//    // check if no bound states in this interval
-//    if (s1 - s0 == 0) {
-//        return vec(uword(0));
-//    }
+    // check if no bound states in this interval
+    if (s1 - s0 == 0) {
+        return vec(uword(0));
+    }
 
-//    unsigned n = 2;
-//    vec E = vec(1025);
-//    ivec s = ivec(1025);
-//    E(0) = E0;
-//    E(1) = E1;
-//    s(0) = s0;
-//    s(1) = s1;
+    unsigned n = 2;
+    vec E = vec(1025);
+    ivec s = ivec(1025);
+    E(0) = E0;
+    E(1) = E1;
+    s(0) = s0;
+    s(1) = s1;
 
-//    unsigned n_bound = 0;
-//    vec E_bound(100);
+    unsigned n_bound = 0;
+    vec E_bound(100);
 
-//    // stack for recursion
-//    std::stack<std::pair<int, int>> stack;
+    // stack for recursion
+    std::stack<std::pair<int, int>> stack;
 
-//    // push first interval to stack
-//    stack.push(std::make_pair(0, 1));
+    // push first interval to stack
+    stack.push(std::make_pair(0, 1));
 
-//    // repeat until all intervals inspected
-//    while (!stack.empty()) {
-//        const auto & i = stack.top();
-//        i0 = i.first;
-//        i1 = i.second;
+    // repeat until all intervals inspected
+    while (!stack.empty()) {
+        const auto & i = stack.top();
+        i0 = i.first;
+        i1 = i.second;
 
-//        stack.pop();
+        stack.pop();
 
-//        // load data
-//        E0 = E(i0);
-//        E1 = E(i1);
-//        s0 = s(i0);
-//        s1 = s(i1);
+        // load data
+        E0 = E(i0);
+        E1 = E(i1);
+        s0 = s(i0);
+        s1 = s(i1);
 
-//        // mid energy
-//        E2 = 0.5 * (E0 + E1);
+        // mid energy
+        E2 = 0.5 * (E0 + E1);
 
-//        // if interval size sufficiently small enough, add new bound state
-//        if (E1 - E0 <= tol) {
-//            if (E_bound.size() <= n_bound) {
-//                E_bound.resize(n_bound * 2);
-//            }
-//            E_bound(n_bound++) = E2;
-//        } else {
-//            // evaluate s at mid energy
-//            s2 = eval(a, a2, b, E2);
+        // if interval size sufficiently small enough, add new bound state
+        if (E1 - E0 <= tol) {
+            if (E_bound.size() <= n_bound) {
+                E_bound.resize(n_bound * 2);
+            }
+            E_bound(n_bound++) = E2;
+        } else {
+            // evaluate s at mid energy
+            s2 = eval(a, a2, b, E2);
 
-//            // add intervals to stack if they contain bound states
-//            if (s1 - s2 > 0) {
-//                stack.push(std::make_pair(n, i1));
-//            }
-//            if (s2 - s0 > 0) {
-//                stack.push(std::make_pair(i0, n));
-//            }
+            // add intervals to stack if they contain bound states
+            if (s1 - s2 > 0) {
+                stack.push(std::make_pair(n, i1));
+            }
+            if (s2 - s0 > 0) {
+                stack.push(std::make_pair(i0, n));
+            }
 
-//            // save E2 and s2
-//            if (E.size() <= n) {
-//                E.resize(2 * n - 1);
-//                s.resize(2 * n - 1);
-//            }
-//            E(n) = E2;
-//            s(n) = s2;
-//            ++n;
-//        }
-//    }
+            // save E2 and s2
+            if (E.size() <= n) {
+                E.resize(2 * n - 1);
+                s.resize(2 * n - 1);
+            }
+            E(n) = E2;
+            s(n) = s2;
+            ++n;
+        }
+    }
 
-//    E_bound.resize(n_bound);
-//    return E_bound;
+    E_bound.resize(n_bound);
+    return E_bound;
 }
 
 template<bool zero_check = true>
@@ -367,71 +367,71 @@ arma::vec charge_density_impl::get_A(const device & d, const potential & phi, co
 }
 
 arma::vec & charge_density_impl::get_n0(const device & d) {
-//    using namespace arma;
+    using namespace arma;
 
-//    auto it = n0_d.find(d.name);
-//    if (it != std::end(n0_d)) {
-//        return it->second;
-//    }
+    auto it = n0_d.find(d.name);
+    if (it != std::end(n0_d)) {
+        return it->second;
+    }
 
-//    vec x0, x1, x2, x3, w0, w1, w2, w3;
+    vec x0, x1, x2, x3, w0, w1, w2, w3;
 
-//    // valence band in contact region
-//    vec nvc = integral([&] (double E) {
-//        double dos = E / sqrt(4*d.tc1*d.tc1*d.tc2*d.tc2 - (E*E - d.tc1*d.tc1 - d.tc2*d.tc2) * (E*E - d.tc1*d.tc1 - d.tc2*d.tc2));
-//        vec ret = arma::vec(2);
-//        ret(0) = (1 - fermi(E, d.F_sc)) * dos;
-//        ret(1) = (1 - fermi(E, d.F_dc)) * dos;
-//        return ret;
-//    }, 2, linspace(E_min, -0.5 * d.E_gc, 100), rel_tol, c::epsilon(), x0, w0);
+    // valence band in contact region
+    vec nvc = integral([&] (double E) {
+        double dos = E / sqrt(4*d.tc1*d.tc1*d.tc2*d.tc2 - (E*E - d.tc1*d.tc1 - d.tc2*d.tc2) * (E*E - d.tc1*d.tc1 - d.tc2*d.tc2));
+        vec ret = arma::vec(2);
+        ret(0) = (1 - fermi(E, d.F_sc)) * dos;
+        ret(1) = (1 - fermi(E, d.F_dc)) * dos;
+        return ret;
+    }, 2, linspace(E_min, -0.5 * d.E_gc, 100), rel_tol, c::epsilon(), x0, w0);
 
-//    // conduction band in contact region
-//    vec ncc = integral([&] (double E) {
-//        double dos = E / sqrt(4*d.tc1*d.tc1*d.tc2*d.tc2 - (E*E - d.tc1*d.tc1 - d.tc2*d.tc2) * (E*E - d.tc1*d.tc1 - d.tc2*d.tc2));
-//        vec ret = arma::vec(2);
-//        ret(0) = fermi(E, d.F_sc) * dos;
-//        ret(1) = fermi(E, d.F_dc) * dos;
-//        return ret;
-//    }, 2, linspace(0.5 * d.E_gc, E_max, 100), rel_tol, c::epsilon(), x1, w1);
+    // conduction band in contact region
+    vec ncc = integral([&] (double E) {
+        double dos = E / sqrt(4*d.tc1*d.tc1*d.tc2*d.tc2 - (E*E - d.tc1*d.tc1 - d.tc2*d.tc2) * (E*E - d.tc1*d.tc1 - d.tc2*d.tc2));
+        vec ret = arma::vec(2);
+        ret(0) = fermi(E, d.F_sc) * dos;
+        ret(1) = fermi(E, d.F_dc) * dos;
+        return ret;
+    }, 2, linspace(0.5 * d.E_gc, E_max, 100), rel_tol, c::epsilon(), x1, w1);
 
-//    // valence band in central region
-//    vec nvsgd = integral([&] (double E) {
-//        double dos = E / sqrt(4*d.t1*d.t1*d.t2*d.t2 - (E*E - d.t1*d.t1 - d.t2*d.t2) * (E*E - d.t1*d.t1 - d.t2*d.t2));
-//        vec ret = arma::vec(3);
-//        ret(0) = (1 - fermi(E, d.F_s)) * dos;
-//        ret(1) = (1 - fermi(E, d.F_g)) * dos;
-//        ret(2) = (1 - fermi(E, d.F_d)) * dos;
-//        return ret;
-//    }, 3, linspace(E_min, - 0.5 * d.E_g, 100), rel_tol, c::epsilon(), x2, w2);
+    // valence band in central region
+    vec nvsgd = integral([&] (double E) {
+        double dos = E / sqrt(4*d.t1*d.t1*d.t2*d.t2 - (E*E - d.t1*d.t1 - d.t2*d.t2) * (E*E - d.t1*d.t1 - d.t2*d.t2));
+        vec ret = arma::vec(3);
+        ret(0) = (1 - fermi(E, d.F_s)) * dos;
+        ret(1) = (1 - fermi(E, d.F_g)) * dos;
+        ret(2) = (1 - fermi(E, d.F_d)) * dos;
+        return ret;
+    }, 3, linspace(E_min, - 0.5 * d.E_g, 100), rel_tol, c::epsilon(), x2, w2);
 
-//    // conduction band in central region
-//    vec ncsgd = integral([&] (double E) {
-//        double dos = E / sqrt(4*d.t1*d.t1*d.t2*d.t2 - (E*E - d.t1*d.t1 - d.t2*d.t2) * (E*E - d.t1*d.t1 - d.t2*d.t2));
-//        vec ret = arma::vec(3);
-//        ret(0) = fermi(E, d.F_s) * dos;
-//        ret(1) = fermi(E, d.F_g) * dos;
-//        ret(2) = fermi(E, d.F_d) * dos;
-//        return ret;
-//    }, 3, linspace(0.5 * d.E_g, E_max, 100), rel_tol, c::epsilon(), x3, w3);
+    // conduction band in central region
+    vec ncsgd = integral([&] (double E) {
+        double dos = E / sqrt(4*d.t1*d.t1*d.t2*d.t2 - (E*E - d.t1*d.t1 - d.t2*d.t2) * (E*E - d.t1*d.t1 - d.t2*d.t2));
+        vec ret = arma::vec(3);
+        ret(0) = fermi(E, d.F_s) * dos;
+        ret(1) = fermi(E, d.F_g) * dos;
+        ret(2) = fermi(E, d.F_d) * dos;
+        return ret;
+    }, 3, linspace(0.5 * d.E_g, E_max, 100), rel_tol, c::epsilon(), x3, w3);
 
-//    // total charge density in contact regions
-//    vec nc = nvc + ncc;
-//    // total charge density in central region
-//    vec nsgd = nvsgd + ncsgd;
+    // total charge density in contact regions
+    vec nc = nvc + ncc;
+    // total charge density in central region
+    vec nsgd = nvsgd + ncsgd;
 
-//    vec ret(d.N_x);
-//    ret(d.sc).fill(nc(0));
-//    ret(d.s).fill(nsgd(0));
-//    ret(d.sox).fill(0);
-//    ret(d.g).fill(nsgd(1));
-//    ret(d.dox).fill(0);
-//    ret(d.d).fill(nsgd(2));
-//    ret(d.dc).fill(nc(1));
+    vec ret(d.N_x);
+    ret(d.sc).fill(nc(0));
+    ret(d.sox).fill(nsgd(0));
+    ret(d.sg).fill(0);
+    ret(d.g).fill(nsgd(1));
+    ret(d.dg).fill(0);
+    ret(d.dox).fill(nsgd(2));
+    ret(d.dc).fill(nc(1));
 
-//    ret *= 2 * c::e / M_PI / M_PI / d.r_cnt / d.dr / d.dx; // spintel inside (?)
+    ret *= 2 * c::e / M_PI / M_PI / d.r_cnt / d.dr / d.dx; // spintel inside (?)
 
-//    n0_d[d.name] = ret;
-//    return n0_d[d.name];
+    n0_d[d.name] = ret;
+    return n0_d[d.name];
 }
 
 #endif
